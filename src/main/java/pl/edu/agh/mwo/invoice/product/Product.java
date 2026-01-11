813d136d299d0e4fd5532533d459ee7441af03a2
zurawskipiotr97-glitch <zurawskipiotr97@gmail.com>
2026-01-11 13:44:57 +0100

    ProductWithNullName + ProductWithNullPrice

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,6 +10,9 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
+        if (name == null || price == null || tax == null ) {
+            throw new IllegalArgumentException();
+        }
         this.name = name;
         this.price = price;
         this.taxPercent = tax;
